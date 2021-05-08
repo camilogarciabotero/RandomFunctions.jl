dfef43125cbeeb7a2b49135613a636080cdb65d8
Camilo García <cgarci39@eafit.edu.co>
2021-05-08 16:46:12 -0500

    tests for new function collatz

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 @testset "RandomFunctions.jl" begin
     @test stop_time(10) == [5, 16, 8, 4, 2, 1]
     @test max_stop_time(10) == (19,9)
+    @test max_stop_time_02(10^7) == (8400511, 685)
 end
