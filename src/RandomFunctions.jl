942a733ca0fe544fcaff4c631bc7845006a2164a
Camilo García <cgarci39@eafit.edu.co>
2021-05-09 11:39:16 -0500

    Add new function

--- src/RandomFunctions.jl
+++ src/RandomFunctions.jl
@@ -4,4 +4,7 @@ module RandomFunctions
 include("collatz.jl")
 export collatz_steps, collatz_steps_02, max_stop_time, max_stop_time_02
 
+include("edit_distance.jl")
+export edit_distance
+
 end
---
+++ src/edit_distance.jl
@@ -0,0 +1,31 @@
+"""
+This functions takes two strings and calculates de edit distance, which is the number
+of changes of two stringsn when both display different lengths:
+
+```@example
+    s1 = "ATCTCGT"
+    s2 = "ACTCCTC"
+    edit_distance(s1,s2)[1] = 3.0
+    edit_distance(s1,s2)[2] =  [0.0 1.0 2.0 3.0 4.0 5.0 6.0 7.0; 
+                               1.0 0.0 1.0 2.0 3.0 4.0 5.0 6.0; 
+                               2.0 1.0 1.0 1.0 2.0 3.0 4.0 5.0; 
+                               3.0 2.0 1.0 2.0 1.0 2.0 3.0 4.0; 
+                               4.0 3.0 2.0 1.0 2.0 2.0 2.0 3.0;
+                               5.0 4.0 3.0 2.0 1.0 2.0 3.0 2.0; 
+                               6.0 5.0 4.0 3.0 2.0 2.0 3.0 3.0;
+                               7.0 6.0 5.0 4.0 3.0 3.0 2.0 3.0]
+```
+This functions outputs the edit distance and the alignment matrix as well.
+"""
+function edit_distance(s1::String, s2::String)
+    D = zeros(length(s1)+1,length(s2)+1)
+    D[:,1]= 0:length(s1) 
+    D[1,:] = 0:length(s2)
+    for i in 2:length(s1)+1
+        for j in 2:length(s2)+1
+            (s1[i-1] != s2[j-1]) ? δ = 1 : δ = 0 
+            D[i,j] = min(D[i-1,j-1]+δ, D[i-1,j]+1, D[i,j-1]+1)
+        end
+    end
+    return D[length(s1)+1, length(s2)+1],D
+end
