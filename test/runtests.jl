befd6db17935228a0d3f9d628b73a4c9e72e0a87
Camilo García <cgarci39@eafit.edu.co>
2021-05-09 11:00:02 -0500

    Add test for

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 @testset "RandomFunctions.jl" begin
     @test collatz_steps(10) == [5, 16, 8, 4, 2, 1]
+    @test collatz_steps_02(10) == [5, 16, 8, 4, 2, 1]
     @test max_stop_time(10) == (19,9)
     @test max_stop_time_02(10^7) == (8400511, 685)
 end
