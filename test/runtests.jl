8736a68713362fd1b7aec8598415a447866a7faf
Camilo García <cgarci39@eafit.edu.co>
2021-05-09 11:38:50 -0500

    Add test for

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
 using RandomFunctions
 using Test
 
-@testset "RandomFunctions.jl" begin
+@testset "collatz.jl" begin
     @test collatz_steps(10) == [5, 16, 8, 4, 2, 1]
     @test collatz_steps_02(10) == [5, 16, 8, 4, 2, 1]
     @test max_stop_time(10) == (19,9)
     @test max_stop_time_02(10^7) == (8400511, 685)
+end
+
+@testset "edit_distance.jl" begin
+    @test edit_distance("ATCTCGT", "ACTCCTC")[1] == 3.0
 end
