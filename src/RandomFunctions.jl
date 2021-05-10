35e3f715c3ee651ec6d927571fbeead109676894
Camilo García <cgarci39@eafit.edu.co>
2021-05-10 17:00:25 -0500

    Include `sieve` on main

--- src/RandomFunctions.jl
+++ src/RandomFunctions.jl
@@ -1,10 +1,12 @@
 module RandomFunctions
 
-# Write your package code here.
 include("collatz.jl")
-export collatz_steps, collatz_steps_02, max_stop_time, max_stop_time_02
+export collatz_steps, collatz_steps_02, max_stop_time, max_stop_time_02, max_stop_time_03
 
 include("edit_distance.jl")
 export edit_distance
+
+include("sieve.jl")
+export sieve
 
 end
