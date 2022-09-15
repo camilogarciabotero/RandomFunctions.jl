bd667bc6e0d4c3851bcbda62127a1e1f990034a6
Camilo García <cgarci39@eafit.edu.co>
2022-09-15 15:24:07 -0500

    update hd function

--- src/hamming_distance.jl
+++ src/hamming_distance.jl
@@ -1,7 +1,7 @@
 function hamming_distance(x::String,y::String)
     @assert length(x) == length(y)
     hd = 0
-    for i in 1:length(x)
+    for i in eachindex(x)
         if x[i] != y[i]
             hd += 1
         end
