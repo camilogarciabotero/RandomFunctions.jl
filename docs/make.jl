7e6ede16bc871596b293829cf7e7c418972ba2e9
Camilo García <cgarci39@eafit.edu.co>
2022-11-13 18:04:32 -0500

    add deploydocs function in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,11 @@ makedocs(
     modules = [RandomFunctions]
 )
 
+deploydocs(
+    repo = "github.com/camilogarciaboter/RandomFunctions.jl.git",
+)
+
+
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
